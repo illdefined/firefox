899b71a46a6818c4be55415cdc2fb988c313a506
Mikael Voss <mvs@nyantec.com>
2025-06-08 10:33:15 +0200

    Fetch smaller nixpkgs from channel

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Custom Firefox build";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "https://channels.nixos.org/nixpkgs-unstable/nixexprs.tar.xz";
   };
 
   nixConfig = {
